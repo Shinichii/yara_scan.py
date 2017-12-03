4fc56dc15ee73888b25998ef3b04d66a369e2ca3
Shinichii <denis.aurelienpl@gmail.com>
2017-12-03 09:44:07 +0100

    Update yara_scan.py
     Made small adjustments

--- yara_scan.py
+++ yara_scan.py
@@ -12,7 +12,7 @@ dirToScanPath = "/home/user/Test" # A remplacer par le dossier desire
 
 print('Downloading latest yara signatures....')
 urllib.urlretrieve(url, "master.zip")
-os.system("unzip master.zip")
+os.system("unzip -o -qq master.zip")
 print('Done')
 print('Loading rules...')
 rules = yara.compile("home/user/rules-master/index.yar")# Choisir l emplacement des regles
@@ -21,11 +21,11 @@ print('Starting scan in ' + dirToScanPath)
 for file in os.listdir(dirToScanPath): #ne fonctionnera pas sur les dossiers
         print(file)
         try:
-                     result = rules.match(dirToScanPath+"/"+file)
-                 if(result == []):
-                       print('Nothing found for this file')
-                 else:
-                       print(result)
-           except:
-                     print('Could not open' + file +  'ignoring it' )
+                result = rules.match(dirToScanPath+"/"+file)
+                if(result == []):
+                        print('Nothing found for this file')
+                else:
+                        print(result)
+        except:
+                print('Could not open' + file +  'ignoring it' )
 print('Scan ended')
